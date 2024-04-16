5b9ede078a05d96941a0a3ee3c7b28ba681a2e80
Marcos <marcos.moretto@gmail.com>
2024-04-15 21:36:41 -0300

    executar npm i para instalar os módulos node_modules

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function App() {
+      const [count, setCount] = useState(0);
+      return (
+        <div>
+          <p>Você clicou {count} vezes</p>
+          <button onClick={() => setCount(count + 1)}>Clique aqui</button>
+        </div>
+      )
+}
+
+export default App;
