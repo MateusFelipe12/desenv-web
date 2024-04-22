45fd329fd1596db584d7f0c69513a43ba3dadf86
Marcos <marcos.moretto@gmail.com>
2024-04-22 20:10:08 -0300

    axios

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,58 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8080/';
+axios.defaults.headers.common["Content-Type"] = 
+      "application/json;charset=utf-8";
 
 function App() {
-      const [count, setCount] = useState(0);
+      const [count, setCountState] = React.useState(0);
+      const [listPosts, setListaPosts] = React.useState([]);
+
+      function counterState(){
+        setCountState(count + 1);
+      }
+
+      React.useEffect(() => {
+        const res = axios.get('blog/api/v1/rest/post');
+        res.then((query)=> {
+          setListaPosts(query.data);
+          console.log(query.data);
+        });
+      })
       return (
         <div>
-          <p>Você clicou {count} vezes</p>
-          <button onClick={() => setCount(count + 1)}>Clique aqui</button>
+          <h1>Home page</h1>
+          <div className='row'>
+            <div className='col-4'>
+              <p>Count com state: {count}</p>
+              <button
+                type='button' className='btn btn-primary'
+                onClick={counterState}>Clique Aqui!</button>
+            </div>
+          </div>
+          <div className='row'>
+            {listPosts.length > 0 && (<table className='table'>
+              <thead>
+                <tr>ID</tr>
+                <tr>Título</tr>
+                <tr>Post</tr>
+              </thead>
+              <tbody>
+                {listPosts.map((post, index) => (
+                  <tr key={index}>
+                    <th key={post.id}>{post.id}</th>
+                    <td key={post.title}><a href={`/post/${post.id}`}>{post.title}</a></td>
+                    <td key={post.post}>{post.post}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            )
+
+            }
+          </div>
         </div>
       )
 }
