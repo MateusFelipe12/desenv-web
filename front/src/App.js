e8c9635d337d4e9e1ed40eb7d313d3dadd0f4be2
Marcos <marcos.moretto@gmail.com>
2024-04-22 21:45:37 -0300

    telas e rotas

--- front/src/App.js
+++ front/src/App.js
@@ -1,60 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
+
+import './App.css';
+import AppRouter from './AppRouter';
 
 axios.defaults.baseURL = 'http://localhost:8080/';
 axios.defaults.headers.common["Content-Type"] = 
       "application/json;charset=utf-8";
 
-function App() {
-      const [count, setCountState] = React.useState(0);
-      const [listPosts, setListaPosts] = React.useState([]);
-
-      function counterState(){
-        setCountState(count + 1);
-      }
-
-      React.useEffect(() => {
-        const res = axios.get('blog/api/v1/rest/post');
-        res.then((query)=> {
-          setListaPosts(query.data);
-          console.log(query.data);
-        });
-      })
-      return (
-        <div>
-          <h1>Home page</h1>
-          <div className='row'>
-            <div className='col-4'>
-              <p>Count com state: {count}</p>
-              <button
-                type='button' className='btn btn-primary'
-                onClick={counterState}>Clique Aqui!</button>
-            </div>
-          </div>
-          <div className='row'>
-            {listPosts.length > 0 && (<table className='table'>
-              <thead>
-                <tr>ID</tr>
-                <tr>Título</tr>
-                <tr>Post</tr>
-              </thead>
-              <tbody>
-                {listPosts.map((post, index) => (
-                  <tr key={index}>
-                    <th key={post.id}>{post.id}</th>
-                    <td key={post.title}><a href={`/post/${post.id}`}>{post.title}</a></td>
-                    <td key={post.post}>{post.post}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-
-            )
-
-            }
-          </div>
-        </div>
-      )
+const App = () => {
+  return (
+    <div>
+      <AppRouter/>
+    </div>
+  )
 }
 
 export default App;
